7528564ae0dd0c44b9be7c012b526ad074a1972a
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-02-15 21:09:05 +0300

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  
+  def initialize(height, base, hypo)
+  @sides=[height, base, hypo]
+  end
+  def kind
+    if(@sides.include?(0))
+        raise TriangleError
+    end
+    if(@sides.find do |i| 
+      i<0
+    end)
+    raise TriangleError
+    end
+    @sides.each.with_index do |s, i|
+      if(@sides[i]>=(@sides[(i+1)%3]+@sides[(i+2)%3]))
+        raise TriangleError
+      end
+
+    end
+
+    if(@sides.uniq.length == 1)
+      return :equilateral
+    elsif(@sides.uniq.length ==2 )
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "you have an error"
+    end
+    
+  end
+
 end
